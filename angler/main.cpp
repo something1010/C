806cfa983d576dad1cb5d4b320cf5ad1e47c801f
Alex Baranov <aleksandr.baranov@wartsila.com>
2023-01-05 19:47:57 +0300

    start programming snake.

--- angler/main.cpp
+++ angler/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include "SDL2/SDL.h"
 #include <map>
@@ -36,39 +37,69 @@ void close(SDL_Surface* helloWorld, SDL_Window* window){
     SDL_Quit();
 }
 
-void magic(){
-    SDL_Event e;
-    enum KeyPressSurfaces
-    {
-        KEY_PRESS_SURFACE_DEFAULT,
-        KEY_PRESS_SURFACE_UP,
-        KEY_PRESS_SURFACE_DOWN,
-        KEY_PRESS_SURFACE_LEFT,
-        KEY_PRESS_SURFACE_RIGHT,
-        KEY_PRESS_SURFACE_TOTAL
-    };
-    std::map<SDL_Keycode, const char *> outList = {
-            {SDLK_UP, "Up"},
-            {SDLK_DOWN, "Dn"},
-            {SDLK_LEFT, "Lt"},
-            {SDLK_RIGHT, "Rt"} };
-    while(true) {
-        SDL_PollEvent(&e);
-        if(e.type == SDL_QUIT) break;
-        if(e.type == SDL_KEYDOWN) std::cout << outList[e.key.keysym.sym] << std::endl;
+struct point{
+    int x = 0;
+    int y = 0;
+};
+struct snake{
+    point sn[1000];
+    int lenght = 1;
+    snake(int x, int y){
+        sn[0].x = x;
+        sn[0].y = y;
+        for (int i = 1; i < 1000; ++i) {
+            sn[i].x = 0;
+            sn[i].y = 0;
+        }
     }
-}
 
+};
 int main(){
-    SDL_Window* window = init("angler sdl", 640, 426);
+    snake s(5, 5);
+    SDL_Window* window = init("angler sdl", 500, 500);
     if (window == nullptr) {return -1;}
+    SDL_Rect fillRect = {s.sn[0].x * 50, s.sn[0].y * 50 ,50, 50};
+    SDL_Renderer* gRenderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
+    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0x00, 0x00, 0xFF );
+    SDL_RenderFillRect( gRenderer, &fillRect );
+    SDL_RenderPresent( gRenderer );
+
+    /*
     SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
     const char* toLoad = "res/l02.bmp";
     SDL_Surface* helloWorld = loadRes(toLoad);
     if (helloWorld == nullptr) {return -1;}
+
     SDL_BlitSurface(helloWorld, nullptr, screenSurface, nullptr );
     SDL_UpdateWindowSurface(window);
-    magic();
-    close(helloWorld, window);
+    */
+
+    SDL_Event e;
+    int f = 0;
+    while(true) {
+        f ++;
+        if (f >= 10){
+            SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+            f = 0;
+            SDL_PollEvent(&e);
+            if (e.type == SDL_QUIT) break;
+
+            if( e.type == SDL_KEYDOWN ) {
+                if (e.key.keysym.sym == SDLK_UP and s.sn[0].y != 0) s.sn[0].y -= 1;
+                if (e.key.keysym.sym == SDLK_RIGHT and s.sn[0].x != 9) s.sn[0].x += 1;
+                if (e.key.keysym.sym == SDLK_LEFT and s.sn[0].x != 0) s.sn[0].x -= 1;
+                if (e.key.keysym.sym == SDLK_DOWN and s.sn[0].y != 9) s.sn[0].y += 1;
+            }
+
+            fillRect = {s.sn[0].x * 50, s.sn[0].y * 50 ,50, 50};
+            SDL_RenderFillRect( gRenderer, &fillRect );
+            SDL_RenderPresent( gRenderer );
+        }
+        if(f == 9){
+            SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF);
+            SDL_RenderClear( gRenderer );
+        }
+    }
+    //close(helloWorld, window);
     return 0;
 }
